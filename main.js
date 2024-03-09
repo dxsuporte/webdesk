cad40bfedcaaea9046a96b19c8994e0dfb575c19
Danilo <dxsuporteti@gmail.com>
2024-03-09 18:58:32 -0300

    primary start

---
+++ main.js
@@ -0,0 +1,44 @@
+//Modules for electron
+const { app, BrowserWindow, nativeImage } = require('electron')
+//Global variables
+const config = require('./config')
+//File and directory
+const path = require('path')
+//Ico Default
+const icon = nativeImage.createFromPath(path.join(__dirname, './img/favicon.png'))
+
+//Electron Window
+const createWindow = () => {
+  const win = new BrowserWindow({
+    icon: icon,
+    minWidth: 1024,
+    minHeight: 768,
+    show: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      devTools: false,
+    },
+  })
+  if (config.HOST) {
+    win.loadURL(`http://${config.HOST}:${config.PORT}`)
+  } else {
+    win.loadFile('index.html')
+  }
+  win.maximize()
+  win.show()
+}
+
+// Iniciar
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+//Fechar
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
